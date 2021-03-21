1989a912e1f65b1e882e69d94f6ec94c4a7a900e
enoch-solano <enochs@seas.upenn.edu>
2021-03-21 01:22:25 -0400

    able to write historical data to a csv file

--- testing.py
+++ testing.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.cbook as cbook
 
 import cbpro
 public_client = cbpro.PublicClient()
+
+import csv
 
 ONE_MINUTE = 60
 ONE_HOUR = 60 * ONE_MINUTE
@@ -26,22 +26,29 @@ HIST_RATES_KEYS = { "time" :    0,  # unix timestamp of bucket start time
 def main():
     # 1) using the CB-API
     hist_rates = public_client.get_product_historic_rates('BTC-USD',
+                                                          start=None, end=None,
                                                           granularity = GRANULARITY["ONE_DAY"])
 
     # 2) parsing the CB-API
     time_data = [np.datetime64(hist_rate[HIST_RATES_KEYS["time"]], 's') for hist_rate in hist_rates]
     price_data = [hist_rate[HIST_RATES_KEYS["close"]] for hist_rate in hist_rates]
 
-
     # 3) make plot
     fig = plt.figure()
     ax = plt.axes()
     ax.plot(time_data, np.array(price_data));
 
-
     # 4) display plot
-    plt.show()
+    # plt.show()
 
+    # 5) store historical data
+    with open('database/btc_historical_data.csv', mode='w+') as csv_file:
+        fieldnames = ['datetime', 'closing_price']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for datetime, price in zip(time_data, price_data):
+            writer.writerow({ 'datetime' : datetime, 'closing_price' : price })
 
 if __name__ == '__main__':
     main()
